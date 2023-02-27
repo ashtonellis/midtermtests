e0a5bb41dda5e903abae73d375d019a547361f47
ASHTON YEACKLEY <aey21@fsu.edu>
2023-02-27 11:04:21 -0500

    add array frm img, add fnct to draw rndm frm array

--- sketch.js
+++ sketch.js
@@ -1,22 +1,38 @@
 let move = false;
 let rock = 0;
+let d1;
+let d2;
+let d3;
+let d4;
+let d5;
+let d6;
+let f1;
+let f2;
+let f3;
+let f4;
+let f5;
+let f6;
+let des = [];
+let fun = [];
+let descheck = [];
+let funcheck = [];
 
 
 function preload() {
   flames1 = loadImage("img/flames1.jpg");
   flames = loadImage("img/flames.gif");
-  des1 = loadImage("img/des1.jpg");
-  des2 = loadImage("img/des2.png");
-  des3 = loadImage("img/des3.jpg");
-  des4 = loadImage("img/des4.JPG");
-  des5 = loadImage("img/des5.jpg");
-  des6 = loadImage("img/des6.JPG");
-  fun1 = loadImage("img/fun1.jpg");
-  fun2 = loadImage("img/fun2.jpg");
-  fun3 = loadImage("img/fun3.jpg");
-  fun4 = loadImage("img/fun4.jpg");
-  fun5 = loadImage("img/fun5.jpg");
-  fun6 = loadImage("img/fun6.jpg");
+  des[0] = loadImage('img/des1.jpg');
+  des[1] = loadImage('img/des1.jpg');
+  des[2] = loadImage('img/des2.png');
+  des[3] = loadImage('img/des3.jpg');
+  des[4] = loadImage('img/des4.JPG');
+  des[5] = loadImage('img/des5.jpg');
+  fun[0] = loadImage('img/fun1.jpg');
+  fun[1] = loadImage('img/fun2.jpg');
+  fun[2] = loadImage('img/fun3.jpg');
+  fun[3] = loadImage('img/fun4.jpg');
+  fun[4] = loadImage('img/fun5.jpg');
+  fun[5] = loadImage('img/fun6.jpg');
   sleep = loadImage("img/sleep.png");
   play = loadImage("img/play.png");
   burn = loadImage("img/burn.png");
@@ -28,6 +44,8 @@ function setup() {
   angleMode(DEGREES);
   imageMode(CENTER);
   deg = 0;
+  descheck = [false, false, false, false, false, false];
+  funcheck = [false, false, false, false, false, false];
 }
 
 function draw() {
@@ -37,143 +55,143 @@ function draw() {
     image(flames1,432.5,375);
   }
 
-  if (rock > 0 && rock < 24) {
+  if (rock > 0) {
     push()
     translate(420,380);
     rotate(-5);
     tint(255,190);
-    image(des3,0,0);
+    image(d1,0,0);
     pop();
   }
 
-  if (rock > 1 && rock < 23) {
+  if (rock > 1) {
     push()
     translate(745,120);
     rotate(30);
     tint(255,190);
-    image(fun1, 0, 0);
+    image(f1, 0, 0);
     pop()
   }
 
-  if (rock > 2 && rock < 22) {
+  if (rock > 2) {
     push()
     translate(290,620);
     rotate(15);
     tint(255,190);
-    image(des2,0,0);
+    image(d2,0,0);
     pop();
   }
 
-  if (rock > 3 && rock < 21) {
+  if (rock > 3) {
     push()
     translate(700,615);
     rotate(-20);
     tint(255,190);
-    image(fun2,0,0);
+    image(f2,0,0);
     pop();
   }
 
-  if (rock > 4 && rock < 20) {
+  if (rock > 4) {
     push()
     translate(175,200);
     rotate(-20);
     tint(255,190);
-    image(des1,0,0);
+    image(d3,0,0);
     pop()
   }
 
-  if (rock > 5 && rock < 19) {
+  if (rock > 5) {
     push()
     translate(125,500);
     rotate(-35);
     tint(255,190);
-    image(fun3,0,0);
+    image(f3,0,0);
     pop();
   }
 
-  if (rock > 6 && rock < 18) {
+  if (rock > 6) {
     push()
     translate(625,430);
     rotate(25);
     tint(255,190);
-    image(des4,0,0);
+    image(d4,0,0);
     pop();
   }
 
-  if (rock > 7 && rock < 17) {
+  if (rock > 7) {
     push()
     translate(500,550);
     rotate(20);
     tint(255,190);
-    image(fun4,0,0);
+    image(f4,0,0);
     pop();
   }
 
-  if (rock > 8 && rock < 16) {
+  if (rock > 8) {
     push()
     translate(420,100);
     rotate(15);
     tint(255,190);
-    image(des5,0,0);
+    image(d5,0,0);
     pop();
   }
 
-  if (rock > 9 && rock < 15) {
+  if (rock > 9) {
     push()
     translate(690,290);
     rotate(-15);
     tint(255,190);
-    image(fun5,0,0);
+    image(f5,0,0);
     pop();
   }
 
-  if (rock > 10 && rock < 14) {
+  if (rock > 10) {
     push()
     translate(70,60);
     rotate(-25);
     tint(255,190);
-    image(des6,0,0);
+    image(d6,0,0);
     pop();
   }
 
-  if (rock > 11 && rock < 13) {
+  if (rock > 11) {
     push()
     translate(190,350);
     rotate(25);
     tint(255,190);
-    image(fun6,0,0);
+    image(f6,0,0);
     pop();
   }
 
-  if (rock > 26 && rock < 34) {
+  if (rock > 12 && rock < 20) {
     push()
     translate(432.5,0);
     image(sleep,0,150);
     pop()
   }
 
-  if (rock > 28 && rock < 36) {
+  if (rock > 13 && rock < 22) {
     push()
     translate(432.5,0);
     image(play,0,300);
     pop()
   }
 
-  if (rock > 30 && rock < 38) {
+  if (rock > 14 && rock < 24) {
     push()
     translate(432.5,0);
     image(burn,0,450);
     pop()
   }
 
-  if (rock > 32 && rock < 40) {
+  if (rock > 15 && rock < 26) {
     push()
     translate(432.5,0);
     image(repeat,0,600);
     pop()
   }
 
-  if (rock > 39) {
+  if (rock > 25) {
     move = false;
     rock = 0;
   }
@@ -184,11 +202,33 @@ function mouseClicked() {
     move = false;
   } else {
     move = true;
+
+    d1 = getRandomItem(des);
+    d2 = getRandomItem(des);
+    d3 = getRandomItem(des);
+    d4 = getRandomItem(des);
+    d5 = getRandomItem(des);
+    d6 = getRandomItem(des);
+    f1 = getRandomItem(fun);
+    f2 = getRandomItem(fun);
+    f3 = getRandomItem(fun);
+    f4 = getRandomItem(fun);
+    f5 = getRandomItem(fun);
+    f6 = getRandomItem(fun);
   }
+}
+// get random item from array (source: programiz.com/javascript/examples/get-random-item)
+function getRandomItem(arr) {
+  const randomIndex = Math.floor(Math.random() * arr.length);
+  const item = arr[randomIndex];
+  return item;
 }
 
 function keyPressed() {
   if (move){
   rock++;
   }
+  console.log(rock);
+  console.log(d1);
+  console.log(d2);
 }
